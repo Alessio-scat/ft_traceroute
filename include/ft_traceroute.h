63b053260d0e5381e38d276db22d66c978905fcb
Alessio-scat <scatamacchia.alessio.lgm@gmail.com>
2024-09-06 16:20:20 +0200

    create socket raw with IPPROTO_UDP standart to macOs/Linux RFC

--- include/ft_traceroute.h
+++ include/ft_traceroute.h
@@ -1,5 +1,15 @@
 #ifndef FT_TRACEROUTE_H
 #define FT_TRACEROUTE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define ERR_CANNOT_CREATE_SOCKET "Error: Cannot create raw socket: %s\n"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
+#include "../include/ft_traceroute.h"
+
+int create_raw_socket()
+{
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+    if (sockfd < 0) {
+        fprintf(stderr, ERR_CANNOT_CREATE_SOCKET, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    return sockfd;
+}
 
 int main ()
 {
-    printf("Traceroute ...\n");
+    int sockfd = create_raw_socket();
+
+    printf("Raw socket (UDP) created successfully\n");
+
+    close(sockfd);
     return 0;
 }
