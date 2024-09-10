fc59472b6cc6561fde96888979cf558301fc0f62
Alessio-scat <scatamacchia.alessio.lgm@gmail.com>
2024-09-10 13:46:45 +0200

    In the subject You have to manage the FQDN without running the DNS resolution in the jump display

---
+++ src/parsing.c
@@ -0,0 +1,98 @@
+#include "../include/ft_traceroute.h"
+
+void print_help()
+{
+	printf("Usage: ping [OPTION...] HOST ...\n"
+	       "Send ICMP ECHO_REQUEST packets to network hosts.\n\n"
+	       " Options:\n"
+	       "  -h or -?           Show help\n"
+	       "  -v                 Verbose output\n");
+        
+    exit(EXIT_SUCCESS);
+}
+
+/*
+    1 : Ip ok
+    0 : Ip invalid
+    -1 : error
+*/
+
+int is_valid_ipv4(const char *ip)
+{
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ip, &(sa.sin_addr));
+
+    if (result == 1)
+        return 1; // IP valid
+    else    
+        return 0;
+}
+
+int is_valid_hostname(const char *hostname) {
+
+    // Regular expression for validating FQDNs
+
+    const char *regex_pattern = "^[a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(\\.[a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+    regex_t regex;
+    int ret;
+    ret = regcomp(&regex, regex_pattern, REG_EXTENDED);
+
+    if (ret) {
+        fprintf(stderr, ERR_REGEX_COMPILATION);
+        exit(EXIT_FAILURE);
+    }
+
+    ret = regexec(&regex, hostname, 0, NULL, 0);
+    regfree(&regex);
+    
+    if (ret == 0)
+        return 1; //Ip valid
+    else
+        return 0;
+
+}
+
+void parse_command_line(int ac, char **av, char **destination, int *verbose) {
+    int opt;
+    int destination_set = 0;
+
+    for (int i = 1; i < ac; i++) {
+        if (av[i][0] == '-') {
+            opt = av[i][1];
+            switch (opt) {
+                case 'v':
+                    *verbose = 1;
+                    break;
+
+                case 'h':
+                    print_help();
+                    break;
+
+                default:
+                    fprintf(stderr, ERR_INVALID_TOS, opt);
+                    exit(EXIT_FAILURE);
+            }
+        } else if (!destination_set) {
+            *destination = av[i];
+            destination_set = 1;
+        } else {
+            fprintf(stderr, ERR_INVALID_ARGUMENT, av[i]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (!destination_set) {
+        fprintf(stderr, ERR_NO_DESTINATION);
+        print_help();
+    }
+
+    // Validate the destination
+
+    if (is_valid_ipv4(*destination) == 1) {} 
+    else if (is_valid_hostname(*destination) == 1) {} 
+    else {
+        fprintf(stderr, ERR_INVALID_DESTINATION, *destination);
+        exit(EXIT_FAILURE);
+    }
+
+}
--- src/resolve_hostname.c
+++ src/resolve_hostname.c
@@ -1,14 +1,21 @@
 #include "../include/ft_traceroute.h"
 
+/*
+    In the subject : 
+        - You have to manage the FQDN without running the DNS resolution in the jump display
+*/
+
 void resolve_hostame_and_display(struct sockaddr_in *src_addr, long rtt)
 {
-    char hostname[NI_MAXHOST]; // NI_MAXHOST standarf POSIX, DNS protocole
-    int err = getnameinfo((struct sockaddr*)src_addr, sizeof(src_addr), hostname, sizeof(hostname), NULL, 0, NI_NAMEREQD);
+    // char hostname[NI_MAXHOST]; // NI_MAXHOST standarf POSIX, DNS protocole
+    // int err = getnameinfo((struct sockaddr*)src_addr, sizeof(src_addr), hostname, sizeof(hostname), NULL, 0, NI_NAMEREQD);
 
-    if (err != 0)
-        strcpy(hostname, inet_ntoa(src_addr->sin_addr));
+    // if (err != 0)
+    //     strcpy(hostname, inet_ntoa(src_addr->sin_addr));
 
+    // if (f_packet == 1)
+    //     printf("%s (%s) ", hostname, inet_ntoa(src_addr->sin_addr));
     if (f_packet == 1)
-        printf("%s (%s) ", hostname, inet_ntoa(src_addr->sin_addr));
+        printf("%s (%s) ", inet_ntoa(src_addr->sin_addr), inet_ntoa(src_addr->sin_addr));
     printf("%ld ms ", rtt); 
 }
